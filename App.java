994d68af8ed650d453f159d16d9b4c15146f9058
Felipe Lantmann Cabreira <felipe.acad@pucrs.br>
2019-02-28 09:41:57 -0300

    feat(Changes AULA 2): feitos exercicios A , B

--- App.java
+++ App.java
@@ -1,14 +1,24 @@
 public class App{
     public static void main(String args[]){
         Placa p1 = new Placa();
+        System.out.println(" ---------- DECLARATIONS ----------- ");
+
         p1.setPais("Brasil");
         p1.setCodigo("ABC1R23");
 
         Veiculo v = new Veiculo(p1);
-        v.abastece(65);
-        v.dirige(250);
-        System.out.println("Dados do veiculo:");
-        System.out.println(v);
-        System.out.println("----------------");
+        // v.abastece(65);
+        // v.dirige(250);
+
+        System.out.println(" -------- EXERCICIOS ( A )--------- ");
+        System.out.println("TOTAL TANK: " + v.getTotalTanque());
+        // System.out.println("ABASTECE: " + v.abastece(151));
+        System.out.println("LITROS COMPRADOS: " + v.abastece(200));
+
+
+        System.out.println(" -------- EXERCICIOS ( B )--------- ");
+        System.out.println("CONSUMO POR LITRO: " + v.verificaConsumoCombustivel());
+        System.out.println("ABASTECE VEICULO: " + v.abastece(100));
+        System.out.println("AUTONOMIA DO VEICULO: " + v.dirige(300));
         }
 }
--- Veiculo.java
+++ Veiculo.java
@@ -1,47 +1,64 @@
-public class Veiculo{
-    private final double consumoPorLitro = 10;
+public class Veiculo {
+    private final double consumoPorLitro = 12;
     private Placa placa;
     private double combustivel;
-    
-    public Veiculo(Placa placa){
+
+    public Veiculo(Placa placa) {
         this.placa = placa;
         combustivel = 0;
     }
 
-    public void setPais(String pais){
+    public void setPais(String pais) {
         placa.setPais(pais);
     }
 
-    public Placa getPlaca(){
+    public Placa getPlaca() {
         return placa;
     }
 
-    public double getCombustivelNoTanque(){
+    public double getCombustivelNoTanque() {
         return combustivel;
     }
 
-    public double abastece(double litros){
-        if (litros > 0.0){
-            combustivel += litros;
+    public double getTotalTanque() {
+        return combustivel = 150;
+    }
+
+    public double verificaConsumoCombustivel() {
+        return consumoPorLitro;
+    }
+
+    public double abastece(double litros) {
+        double litrosExcedidos = 0;
+        if (litros < getTotalTanque() && litros > 0.0) {
+            // if (litros > 0.0) { // Valida Numero Negativo
+                combustivel += litros;
+            // }
+        } else {
+            litrosExcedidos = litros - combustivel;
+            System.out.println("LITROS EXCEDIDOS ( NÃO CONTABILIZADOS ): " + litrosExcedidos);
+            System.out.println("LITROS ABASTECIDOS : " + combustivel);
         }
         return litros;
     }
 
-    public double dirige(double distancia){
+    public double dirige(double distancia) {
         // Calcula o consumo para a distancia
-        double consumo = distancia / consumoPorLitro;
+        double consumo = (distancia / verificaConsumoCombustivel());
         // Diminui o combustivel gasto e retorna a distancia percorrida
-        if (combustivel >= consumo){
+        if (combustivel >= consumo) {
             combustivel -= consumo;
+            System.out.println("combustivel" + combustivel);
             return distancia;
-        }else{
-            double distPossivel = combustivel * consumoPorLitro;
+        } else {
+            double distPossivel = combustivel * verificaConsumoCombustivel();
             combustivel = 0.0;
+            System.out.println("distPossivel" + distPossivel);
             return distPossivel;
         }
     }
 
-    public String toString(){
-        return "Placa: "+getPlaca()+", combustivel no tanque: "+getCombustivelNoTanque();
+    public String toString() {
+        return "Placa: " + getPlaca() + ", combustivel no tanque: " + getCombustivelNoTanque();
     }
 }
